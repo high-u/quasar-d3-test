460c713c16454521086348e0a49a0aa6517350f8
high-u <zen.crazyd@gmail.com>
2017-12-18 02:04:10 +0900

    X Axis. scalePoint to scaleTime.

--- src/components/ChartArea.vue
+++ src/components/ChartArea.vue
@@ -34,8 +34,13 @@ export default {
         .attr('width', width + margin.left + margin.right) // set its dimentions
         .attr('height', height + margin.top + margin.bottom)
 
-      var xScale = d3.scalePoint()
-        .domain(dataset.map(function (v) { return v.label }))
+      var timeparser = d3.timeParse('%Y-%m-%d')
+      dataset = dataset.map(function (d) {
+        return { label: timeparser(d.label), count: d.count }
+      })
+
+      var xScale = d3.scaleTime()
+        .domain([d3.min(dataset.map(function (d) { return d.label })), d3.max(dataset.map(function (d) { return d.label }))])
         .range([0, width])
 
       var yScale = d3.scaleLinear()
@@ -44,6 +49,7 @@ export default {
 
       /* x is the d3.scaleTime() */
       var xAxis = d3.axisBottom(xScale)
+        .tickFormat(d3.timeFormat('%m/%e'))
       /* .ticks(4) // specify the number of ticks */
       var yAxis = d3.axisLeft(yScale)
       /* .tickFormat(d3.format('$,d')) */
